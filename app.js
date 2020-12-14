668992ff51a4a95c60d5df2c6584fdfb01f566fd
eendo01 <elizabeth.endo@tufts.edu>
2020-12-14 11:35:42 -0500

    pls

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.listen(process.env.PORT || 3000, function() {
 })
 
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
