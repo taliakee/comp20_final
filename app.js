ad48697b58cacda21ed01a02415592f4606351cc
taliakee <taliakee@gmail.com>
2020-12-13 20:39:24 -0500

    adding app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('server is running')
+})
+
+app.get('/', (req, res) => {
+    res.send("This will have our index page");
+})
+
+app.get("/hoursandlocations", (req, res) => {
+    res.sendFile(__dirname, '/hoursandlocations.html');
+})
+
